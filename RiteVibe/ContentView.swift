74cca3bbe1260c93c3bdcf5e7308bb83727c9a47
Ahsan Minhas <ahsan.minhas.official@gmail.com>
2026-05-22 18:01:53 +0500

    Initial Commit

---
+++ RiteVibe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RiteVibe
+//
+//  Created by Ahsan Minhas on 22/05/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RiteVibe/RiteVibeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RiteVibeApp.swift
+//  RiteVibe
+//
+//  Created by Ahsan Minhas on 22/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct RiteVibeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
